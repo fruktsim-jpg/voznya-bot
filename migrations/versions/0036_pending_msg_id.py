44aedf3146e7c832dfc9d2af3063ab54f6efcfc3
fruktsim-jpg <frukts.im@gmail.com>
2026-06-10 05:47:13 +0200

    feat: Reintroduce pending action request message migration for cleanup

--- migrations/versions/0036_pending_msg_id.py
+++ migrations/versions/0036_pending_msg_id.py
@@ -1,6 +1,6 @@
 """Store duel request message ids for cleanup.
 
-Revision ID: 0036_pending_action_request_message
+Revision ID: 0036_pending_msg_id
 Revises: 0035_seed_season_1
 Create Date: 2026-06-10
 """
@@ -10,7 +10,7 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "0036_pending_action_request_message"
+revision: str = "0036_pending_msg_id"
 down_revision: Union[str, None] = "0035_seed_season_1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
